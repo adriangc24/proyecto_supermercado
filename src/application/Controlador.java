baa72bba516574c21867c0e6508af13aa043f9ca
adriangc24 <adriangcamacho24@gmail.com>
2020-02-03 18:34:24 +0100

    ok

--- src/application/Controlador.java
+++ src/application/Controlador.java
@@ -3,14 +3,33 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class Controlador implements Initializable{
-
+	@FXML
+	private TextField txtUser;
+	@FXML
+	private TextField txtPass;
+	@FXML
+	public Button botonLogin;
+	@FXML
+	private ImageView imgLogin;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 		
 	}
+	public void onClick() {
+		imgLogin.setOpacity(0);
+	}
+	public void onRelease() {
+		imgLogin.setOpacity(100);
+	}
+	
 
 }
